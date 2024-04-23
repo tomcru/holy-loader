a873138f9dcd2b726b085cf7abb3febf7f70a89f
lorikku <lorikkonjusevci@gmail.com>
2024-04-23 11:06:14 +0200

    Added check to prevent startTrickle from firing multiple times

--- src/HolyProgress.ts
+++ src/HolyProgress.ts
@@ -184,15 +184,15 @@ export class HolyProgress {
    * @public
    * @returns {HolyProgress} The current instance for chaining methods.
    */
-  public start = (): HolyProgress => {
+  public readonly start = (): HolyProgress => {
     if (this.status === null) {
       this.setTo(0);
-    }
 
-    this.startTrickle();
+      this.startTrickle();
 
-    if (this.settings.showSpinner === true) {
-      this.createSpinner();
+      if (this.settings.showSpinner === true) {
+        this.createSpinner();
+      }
     }
 
     return this;
@@ -219,7 +219,7 @@ export class HolyProgress {
    * @public
    * @returns {HolyProgress} The current instance for chaining methods.
    */
-  public complete = (): HolyProgress => this.setTo(1);
+  public readonly complete = (): HolyProgress => this.setTo(1);
 
   /**
    * Calculates an increment value based on the current status of the progress.
