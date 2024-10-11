2ccf57c7256b000d7cf278e654618e61b8db3b90
Tom Rumpf <35841182+tomcru@users.noreply.github.com>
2024-10-11 12:31:11 +0200

    chore: use `.download` instead of `.hasAttribute('download')`

--- src/index.tsx
+++ src/index.tsx
@@ -195,7 +195,7 @@ const HolyLoader = ({
         if (
           anchor === null ||
           anchor.target === '_blank' ||
-          anchor.hasAttribute('download') ||
+          anchor.download ||
           event.ctrlKey ||
           event.metaKey ||
           // Skip if URL points to a different domain
